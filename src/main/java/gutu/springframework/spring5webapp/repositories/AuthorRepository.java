24bd772520ea990fff2092369169a5bae05fea63
Vinicius Costa <viniciuscs@ciandt.com>
2017-11-07 11:20:10 -0200

    New repositories

---
+++ src/main/java/gutu/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,9 @@
+package gutu.springframework.spring5webapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import gutu.springframework.spring5webapp.model.Author;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+
+}
